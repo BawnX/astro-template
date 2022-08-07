d39aec003e54707bfba0cc73afa07229d7134a7a
Antonio Morales <antonio.morales@anschile.cl>
2022-08-07 18:39:44 -0400

    feat: added i18 and vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
+  integrations: [vue(), tailwind()]
 });
